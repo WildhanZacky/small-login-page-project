dd355250d8fed580f5a46e39ef2a3f10605e00ef
wildanzacky <wildanzacky67@gmail.com>
2026-04-03 09:21:11 +0700

    Clock & date updt

--- script2.js
+++ script2.js
@@ -8,3 +8,34 @@ function navigateTo(url) {
         window.location.href = url
     }, 300);
 }
+
+function updateClock() {
+    const now = new Date();
+
+    let h = now.getHours();
+    let m = now.getMinutes();
+    let s = now.getSeconds();
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    document.getElementById('hours').textContent = h;
+    document.getElementById('minutes').textContent = m;
+    document.getElementById('seconds').textContent = s;
+
+    let message = "";
+    if (h >= 5 && h < 12) message = "Good Morning 🌅"
+    else if (h >= 12 && h < 15) message = "Good Afternoon ☀️"
+    else if (h >= 15 && h < 18) message = "Good Afternoon 🌇"
+    else message = "Good Evening 🌙"
+
+    document.getElementById('greeting').innerText = message;
+
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    document.getElementById('date-display').innerText = now.toLocaleDateString('id-ID', options);
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
